15ced7b92111265573f3a57bde524bed65c12c89
dePaula06 <p.depaula06.sp@gmail.com>
2025-04-14 22:23:12 -0300

    specific changes to the navbar styling, changing the image of a guest of honor and assigning a link to the knowEquipes button

--- js/script.js
+++ js/script.js
@@ -414,7 +414,7 @@ function reloading() {
         "../img/imgMascoteVermelho.png",
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
         "Diego Galante, atleta olímpico e instrutor de jiu-jitsu, hexacampeão brasileiro e bicampeão mundial.",
-        "../img/imgHomenageadoVermelho.png",
+        "../img/imgHomenageadoVermelho.jpeg",
         "Diego Galante"
       )
   }
